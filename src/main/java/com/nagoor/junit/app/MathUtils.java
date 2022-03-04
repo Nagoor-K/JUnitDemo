adc0ef2cc633a5bef5ed6007b63bd06e89076a54
Nagoor-K <p.nagoor@axelor.com>
2022-03-04 12:27:01 +0530

    implemeting asserton and test life cycle annotations

--- src/main/java/com/nagoor/junit/app/MathUtils.java
+++ src/main/java/com/nagoor/junit/app/MathUtils.java
@@ -6,16 +6,16 @@ public class MathUtils {
 		return a+b;
 	}
 	public int subNums(int a, int b) {
-		return a+b;
+		return a-b;
 	}
 	public int mulNums(int a, int b) {
-		return a+b;
+		return a*b;
 	}
 	public int divNums(int a, int b) {
 		return a/b;
 	}
 	
-	public double area(double a, double b) {
-		return 1/2*a*b;
+	public String name(String str) {
+		return str+" Nagoor Khan";
 	}
 }
--- src/test/java/com/nagoor/junit/app/MathUtilsTest.java
+++ src/test/java/com/nagoor/junit/app/MathUtilsTest.java
@@ -2,22 +2,58 @@ package com.nagoor.junit.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class MathUtilsTest {
-
+    MathUtils mathUtils;
+    
+    @BeforeAll
+    static void testBeforeAll() {
+    	System.out.print("This will run before all the methods");
+    }
+    
+    @Test
+    @Disabled //to disable this method
+    @DisplayName("Method will fail automatically")
+    void test() {
+    	fail("Method will fail");
+    }
+    
+    @BeforeEach
+	void init() {
+		mathUtils=new MathUtils();
+	}
+    
+    @AfterEach
+    void testClean() {
+    	System.out.print("This will run after each method");
+    }
+    
   @Test
-  void test() {
-    MathUtils mathUtils=new MathUtils();
+  @DisplayName("Testing Add Method")
+  void testAdd() {
     int expected=2;
     int actual=mathUtils.addNums(1, 1);
     assertEquals(expected, actual);
   }
   
   @Test
+  @DisplayName("Testing Divide Method")
   void testDivide() {
-    MathUtils mathUtils=new MathUtils();
     assertThrows(ArithmeticException.class, () -> mathUtils.divNums(1, 0),"Divded by zero");
+  }
+  @Test
+  @DisplayName("Testing Name Method")
+  void testName() {
+    String expected="PatanNagoorKhan";
+    assertNotEquals(expected, mathUtils.name("Patan"), "The test case failed ");
   }
   
 }
